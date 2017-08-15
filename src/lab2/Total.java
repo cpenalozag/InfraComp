f3408ec1f55e94bde1295d05ba9ef01bb61e25a2
CARLOS GABRIEL PEÑALOZA GOMEZ <c.penaloza@sis.virtual.uniandes.edu.co>
2017-08-15 11:37:50 -0500

    Comienzo lab 2

---
+++ src/lab2/Total.java
@@ -0,0 +1,26 @@
+package lab2;
+
+public class Total {
+	
+	private int nThreads;
+	private int total;
+	
+	public Total (int n){
+		nThreads = n;
+		total = 0;
+	}
+	
+	public synchronized void anotar(int pSuma){
+		total += pSuma;
+		nThreads--;
+	}
+	
+	public boolean termino(){
+		return nThreads==0;
+	}
+	
+	public int darTotal()
+	{
+		return total;
+	}
+}
